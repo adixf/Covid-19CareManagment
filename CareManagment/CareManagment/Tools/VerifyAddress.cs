8966444a90c1ba1c188dfb9f9403bf58a086dbc4
rachelis19 <44132906+rachelis19@users.noreply.github.com>
2020-09-10 15:27:57 +0300

    address

--- CareManagment/CareManagment/Tools/VerifyAddress.cs
+++ CareManagment/CareManagment/Tools/VerifyAddress.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareManagment.DP;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,42 @@ namespace CareManagment.Tools
 {
     class VerifyAddress
     {
-        public bool IsValidAddress()
+        public bool IsRealAdrress(Address address)
         {
-            return true;
+            JObject contentAsJSON = GetAdderssJson(address);
+            string street = contentAsJSON["results"][0]["locations"][0]["street"].ToString();
+
+            return street.Contains(address.Street);
+        }
+
+        public double[] GetLatLongFromAddress(Address address)
+        {
+            JObject contentAsJSON = GetAdderssJson(address);
+            double latitude = double.Parse(contentAsJSON["results"][0]["locations"][0]["latLng"]["lat"].ToString());
+            double longitude = double.Parse(contentAsJSON["results"][0]["locations"][0]["latLng"]["lng"].ToString());
+            return new double[] { latitude, longitude };
+        }
+
+        private JObject GetAdderssJson(Address address)
+        {
+            string location = address.ToString() + ", ישראל";
+            string KEY = @"5rRMSAOyU11mGgkbAlWk3C1y1y0nT2Gv";
+
+            var client = new RestClient("http://www.mapquestapi.com/geocoding/v1/address");
+            client.Timeout = -1;
+
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("key", KEY);
+            request.AddParameter("location", location);
+            request.AddParameter("maxResults", 1);
+            request.AddParameter("thumbMaps", false);
+
+
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            object deserializeContent = JsonConvert.DeserializeObject<object>(response.Content);
+            return JObject.Parse(deserializeContent.ToString());
         }
     }
 }
